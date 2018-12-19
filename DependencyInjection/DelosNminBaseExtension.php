11a82872eae9629913cbe46b3910741f4c9bfc5d
Carsten Kollmeier <kollmeier@odas.biz>
2018-12-19 11:23:55 +0100

    Naming Extension class

--- DependencyInjection/DelosNminBaseExtension.php
+++ DependencyInjection/DelosNminBaseExtension.php
@@ -14,7 +14,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class DelosBaseExtension extends Extension
+class DelosNminBaseExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
